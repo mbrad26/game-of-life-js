82ea751a4d5b76903014af962e6e58994621d0b0
mbrad26 <mbrad26@gmail.com>
2020-12-23 13:05:55 +0000

    dies with less than two live neighbours

--- spec/cell.test.js
+++ spec/cell.test.js
@@ -18,4 +18,17 @@ describe('Cell', () => {
 
     expect(cell.state).toEqual(1);
   });
+
+  describe('newState', () => {
+    it('dies with less than two live neighbours', () => {
+      const neighbours = 1;
+      const cell = new Cell('ALIVE');
+
+      expect(cell.state).toEqual(1);
+
+      cell.newState(2);
+
+      expect(cell.state).toEqual(0);
+    });
+  });
 });
