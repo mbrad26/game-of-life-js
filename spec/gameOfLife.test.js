4f5473df748ca53d9a0a47949a1aaa86df741363
mbrad26 <mbrad26@gmail.com>
2020-12-28 13:28:23 +0000

    refactor test suits

--- spec/gameOfLife.test.js
+++ spec/gameOfLife.test.js
@@ -27,17 +27,21 @@ describe('GameOfLife', () => {
   });
 
   describe('.getNeighbours', () => {
+    let spy;
+    const grid = [
+      ['', 'ALIVE', '', ''], 
+      ['', '', '', 'ALIVE'], 
+      ['', '', 'ALIVE', ''],
+      ['', 'ALIVE', '', '']
+    ];
+
+    beforeEach(() => {
+      spy = jest.fn(str => { return { state: str === 'ALIVE' ? 1: 0}});
+    })
+    
     it('returns the number of alive neighbours', () => {
       const row = 1;
       const col = 1;
-      const grid = [
-        ['', 'ALIVE', '', ''], 
-        ['', '', '', 'ALIVE'], 
-        ['', '', 'ALIVE', ''],
-        ['', 'ALIVE', '', '']
-      ];
-
-      const spy = jest.fn(str => { return { state: str === 'ALIVE' ? 1: 0}});
       const game = new GameOfLife(grid, spy);
       
       expect(spy).toHaveBeenCalledTimes(16);
@@ -47,18 +51,9 @@ describe('GameOfLife', () => {
     it('returns the correct number of alive neighbours for cell at row 1, col 0', () => {
       const row = 1;
       const col = 0;
-      const grid = [
-        ['', 'ALIVE', '', ''], 
-        ['', '', '', 'ALIVE'], 
-        ['', '', 'ALIVE', ''],
-        ['', 'ALIVE', '', '']
-      ];
-
-      const spy = jest.fn(str => { return { state: str === 'ALIVE' ? 1: 0}});
       const game = new GameOfLife(grid, spy);
 
-      console.log('COUNT: ', game.grid);
-
+      expect(spy).toHaveBeenCalledTimes(16);
       expect(game.getNeighbours(row, col)).toEqual(1);
     });
   });
