f6ab56e1fca6779fa9c937c58d5e2d6f4132f57d
mbrad26 <mbrad26@gmail.com>
2020-12-28 12:34:06 +0000

    .getNeighbours returns the number of alive neighbours

--- spec/gameOfLife.test.js
+++ spec/gameOfLife.test.js
@@ -1,18 +1,20 @@
 const GameOfLife = require('../lib/gameOfLife');
 const Cell = require('../lib/cell');
 
-jest.mock('../lib/cell', () => {
-  return jest.fn();
-});
+jest.mock('../lib/cell');
 
 describe('GameOfLife', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
   it('is defined', () => {
     expect(GameOfLife).toBeDefined();
   });
 
   it('initializes with a nested array representing columns and rows', () => {
-    const state = new Array(3).fill(new Array(3).fill(''));
-    const game = new GameOfLife(state, Cell);
+    const grid = new Array(3).fill(new Array(3).fill(''));
+    const game = new GameOfLife(grid, Cell);
 
     expect(Cell).toHaveBeenCalledTimes(9);
     expect(game.grid).toEqual(
@@ -22,5 +24,24 @@ describe('GameOfLife', () => {
         [new Cell(''), new Cell(''), new Cell('')]
       ]
     );
+  });
+
+  describe('.getNeighbours', () => {
+    it('returns the number of alive neighbours', () => {
+      const row = 1;
+      const col = 1;
+      const grid = [
+        ['', 'ALIVE', '', ''], 
+        ['', '', '', 'ALIVE'], 
+        ['', '', 'ALIVE', ''],
+        ['', 'ALIVE', '', '']
+      ];
+
+      const spy = jest.fn(str => { return { state: str === 'ALIVE' ? 1: 0}})
+      const game = new GameOfLife(grid, spy);
+      
+      expect(spy).toHaveBeenCalledTimes(16);
+      expect(game.getNeighbours(row, col)).toEqual(2);
+    });
   });
 });
